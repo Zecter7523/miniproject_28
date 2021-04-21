170508cdad151a96534facc560cbc76b94b367f4
Zecter7523 <zanetomlinson98@gmail.com>
2021-04-21 09:43:53 -0500

    installed nod mods

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS travel_db;
+
+CREATE DATABASE travel_db;
+
+USE travel_db
+
+-- CREATE TABLE users(
+--     id INTEGER NOT NULL,
+--     name VARCHAR(30),
+--     email VARCHAR(30),
+
+--     PRIMARYKEY(id);
+-- );
+
+-- CREATE TABLE location(
+--     id INTEGER NOT NULL,
+--     name VARCHAR(30),
+
+--     PRIMARYKEY(id);
+-- );
+
+-- CREATE TABLE trips(
+-- id INTEGER NOT NULL,
+-- trip_budget INTEGER(5,2),
+-- travelerID
+-- );
+
